a73c8dfb0bf937202ed0a5ba0507f88e9b9697e3
Danilo Braga <dannaril@gmail.com>
2015-06-24 22:59:58 -0300

    change package

--- SpringBatch/src/main/java/com/dbraga/springbat/main/App.java
+++ SpringBatch/src/main/java/com/dbraga/springbat/main/App.java
@@ -1,4 +1,4 @@
-package com.dbraga.springbat;
+package com.dbraga.springbat.main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
